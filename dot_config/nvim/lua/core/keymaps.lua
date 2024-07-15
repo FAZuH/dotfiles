72cccce57edee8b08cd885f6cd35387ad8f58f74
FAZuH <fazuhhh@gmail.com>
2024-07-15 18:01:19 +0700

    Add nvim plugin moltens. Modify nvim keymaps

--- dot_config/nvim/lua/core/keymaps.lua
+++ dot_config/nvim/lua/core/keymaps.lua
@@ -2,72 +2,90 @@ local M = {}
 
 vim.g.mapleader = " "
 
-local keymap = vim.keymap
+local keys = vim.keymap
 
--- debugger
-keymap.set("n", "<leader>d", "", { desc = "Debugging" })
-keymap.set("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", { desc = "Debug: Toggle debugging UI" })
-keymap.set("n", "<leader>db", ":DapToggleBreakpoint<CR>", { desc = "Debug: Toggle breakpoint" })
-keymap.set("n", "<leader>dr", ":lua require('dapui').open({ reset = true })<CR>", { desc = "Debug: Reset UI" })
-keymap.set("n", "<leader>dc", ":DapContinue<CR>", { desc = "Debug: Resume program" })
-keymap.set("n", "<leader>ds", ":DapStepOver<CR>", { desc = "Debug: Step over" })
-keymap.set("n", "<leader>di", ":DapStepInto<CR>", { desc = "Debug: Step into" })
-keymap.set("n", "<leader>do", ":DapStepOut<CR>", { desc = "Debug: Step out" })
-keymap.set("n", "<leader>dm", ":lua require('neotest').run.run()<cr>", { desc = "Test Method" })
-keymap.set("n", "<leader>dM", ":lua require('neotest').run.run({strategy = 'dap'})<cr>", { desc = "Test Method DAP" })
-keymap.set("n", "<leader>df", ":lua require('neotest').run.run({vim.fn.expand('%')})<cr>", { desc = "Test Class" })
-keymap.set(
+-- Group naming
+keys.set("n", "<leader>d", "", { desc = "Debugging" })
+keys.set("n", "<leader>f", "", { desc = "Find" })
+keys.set("n", "z", "", { desc = "fold" })
+keys.set("n", "<leader>l", "", { desc = "LSP" })
+-- keys.set("n", "g", "", { desc = "Movement:Go" })
+keys.set("n", "m", "", { desc = "Marks" })
+keys.set("n", "]", "", { desc = "Movement:Next" })
+keys.set("n", "[", "", { desc = "Movement:Previous" })
+keys.set("n", "<leader>p", "", { desc = "Python" })
+keys.set("n", "<leader>m", "", { desc = "Python:Molten" })
+keys.set("n", "<leader>w", "", { desc = "Session" })
+keys.set("n", "<leader>t", "", { desc = "Tabs" })
+keys.set("n", "<leader>s", "", { desc = "Windows" })
+
+-- Debugger
+keys.set("n", "<leader>dt", ":lua require('dapui').toggle()<CR>", { desc = "Debug: Toggle debugging UI" })
+keys.set("n", "<leader>db", ":DapToggleBreakpoint<CR>", { desc = "Debug: Toggle breakpoint" })
+keys.set("n", "<leader>dr", ":lua require('dapui').open({ reset = true })<CR>", { desc = "Debug: Reset UI" })
+keys.set("n", "<leader>dc", ":DapContinue<CR>", { desc = "Debug: Resume program" })
+keys.set("n", "<leader>ds", ":DapStepOver<CR>", { desc = "Debug: Step over" })
+keys.set("n", "<leader>di", ":DapStepInto<CR>", { desc = "Debug: Step into" })
+keys.set("n", "<leader>do", ":DapStepOut<CR>", { desc = "Debug: Step out" })
+keys.set("n", "<leader>dm", ":lua require('neotest').run.run()<cr>", { desc = "Test Method" })
+keys.set("n", "<leader>dM", ":lua require('neotest').run.run({strategy = 'dap'})<cr>", { desc = "Test Method DAP" })
+keys.set("n", "<leader>df", ":lua require('neotest').run.run({vim.fn.expand('%')})<cr>", { desc = "Test Class" })
+keys.set(
   "n",
   "<leader>dF",
   ":lua require('neotest').run.run({vim.fn.expand('%'), strategy = 'dap'})<cr>",
   { desc = "Test Class DAP" }
 )
-keymap.set("n", "<leader>dS", ":lua require('neotest').summary.toggle()<cr>", { desc = "Test Summary" })
+keys.set("n", "<leader>dS", ":lua require('neotest').summary.toggle()<cr>", { desc = "Test Summary" })
 
--- misc
-keymap.set("i", "kj", "<Esc>")
-keymap.set("n", "<leader>+", "<C-a>", { desc = "increment number" })
-keymap.set("n", "<leader>-", "<C-x>", { desc = "decrement number" })
-keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "clear search highlights" })
-keymap.set("n", "<C-s>", ":w<CR>", { noremap = true })
-keymap.set("i", "<C-s>", "<C-o>:write<CR>a", { noremap = true })
+-- Misc
+keys.set("i", "kj", "<Esc>")
+keys.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keys.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+keys.set("n", "<C-s>", ":w<CR>", { noremap = true })
+keys.set("i", "<C-s>", "<C-o>:write<CR>a", { noremap = true })
 
--- python
-keymap.set("n", "<leader>p", "", { desc = "Python" })
-keymap.set("n", "<leader>po", ":PyrightOrganizeImports<CR>", { desc = "Python: Organize imports" })
-keymap.set(
+-- Python
+keys.set("n", "<leader>po", ":PyrightOrganizeImports<CR>", { desc = "Python: Organize imports" })
+keys.set(
   "n",
   "<leader>pp",
   ":PyrightSetPythonPath .venv/bin/python<CR>",
   { desc = "Python: Set python path to workdir's .venv" }
 )
+-- Python:molten
+keys.set("n", "<leader>mi", ":MoltenInit<CR>", { silent = true, desc = "Initialize the plugin" })
+keys.set("n", "<leader>me", ":MoltenEvaluateOperator<CR>", { silent = true, desc = "Run operator selection" })
+keys.set("n", "<leader>mr", ":MoltenEvaluateLine<CR>", { silent = true, desc = "Evaluate line" })
+keys.set("n", "<leader>mR", ":MoltenReevaluateCell<CR>", { silent = true, desc = "Re-evaluate cell" })
+keys.set("v", "<leader>mv", ":<C-u>MoltenEvaluateVisual<CR>gv", { silent = true, desc = "Evaluate visual selection" })
+keys.set("n", "<leader>md", ":MoltenDelete<CR>", { silent = true, desc = "Molten delete cell" })
+keys.set("n", "<leader>mh", ":MoltenHideOutput<CR>", { silent = true, desc = "Hide output" })
+keys.set("n", "<leader>ms", ":noautocmd MoltenEnterOutput<CR>", { silent = true, desc = "Show/enter output" })
 
--- search
-keymap.set("n", "<leader>t", "", { desc = "Search & Find" })
-keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "fuzzy find files in cwd" })
-keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "fuzzy find recent files" })
-keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "find string in cwd" })
-keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "find string under cursor in cwd" })
-keymap.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "find todos" })
+-- Find
+keys.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+keys.set("n", "<leader>fh", ":nohl<CR>", { desc = "Clear search highlights" })
+keys.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+keys.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+keys.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+keys.set("n", "<leader>ft", "<cmd>TodoTelescope<cr>", { desc = "Find todos" })
 
--- tab management
-keymap.set("n", "<leader>t", "", { desc = "Tabs" })
-keymap.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "open new tab" })
-keymap.set("n", "<leader>tl", "<cmd>tabclose<CR>", { desc = "close current tab" })
-keymap.set("n", "L", "<cmd>tabn<CR>", { desc = "go to next tab" })
-keymap.set("n", "H", "<cmd>tabp<CR>", { desc = "go to prev tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "open current buffer in new tab" })
+-- Tab management
+keys.set("n", "<leader>tt", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keys.set("n", "<leader>tl", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keys.set("n", "L", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keys.set("n", "H", "<cmd>tabp<CR>", { desc = "Go to prev tab" })
+keys.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
--- window management
-keymap.set("n", "<leader>s", "", { desc = "Windows" })
-keymap.set("n", "<leader>ss", ":vsplit<CR>", { desc = "split window vertically" })
-keymap.set("n", "<leader>sh", ":split<CR>", { desc = "split window horizontally" })
-keymap.set("n", "<leader>sv", "<C-w>=", { desc = "make windows equal size" })
-keymap.set("n", "<leader>sl", "<cmd>close<CR>", { desc = "close current split" })
+-- Window management
+keys.set("n", "<leader>ss", ":vsplit<CR>", { desc = "Split window vertically" })
+keys.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
+keys.set("n", "<leader>sv", "<C-w>=", { desc = "Make windows equal size" })
+keys.set("n", "<leader>sl", "<cmd>close<CR>", { desc = "Close current split" })
 
 -- LSP
-keymap.set("n", "<leader>l", "", { desc = "LSP" })
-keymap.set(
+keys.set(
   "n",
   "<leader>lc",
   ":lua require('neogen').generate()<CR>",
@@ -77,35 +95,35 @@ function M.lsp_attach_callback(ev)
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   local opts = { buffer = ev.buf, silent = true }
 
-  opts.desc = "show documentation for what is under cursor"
-  keymap.set("n", "<leader>l?", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
+  opts.desc = "Show documentation for what is under cursor"
+  keys.set("n", "<leader>l?", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
-  opts.desc = "go to declaration"
-  keymap.set("n", "<leader>lT", vim.lsp.buf.declaration, opts) -- go to declaration
-  opts.desc = "show LSP definitions"
-  keymap.set("n", "<leader>ly", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
-  opts.desc = "show LSP implementations"
-  keymap.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
+  opts.desc = "Go to declaration"
+  keys.set("n", "<leader>lT", vim.lsp.buf.declaration, opts) -- go to declaration
+  opts.desc = "Show LSP definitions"
+  keys.set("n", "<leader>ly", "<cmd>Telescope lsp_definitions<CR>", opts) -- show lsp definitions
+  opts.desc = "Show LSP implementations"
+  keys.set("n", "<leader>li", "<cmd>Telescope lsp_implementations<CR>", opts) -- show lsp implementations
 
-  opts.desc = "smart rename"
-  keymap.set("n", "<leader>ln", vim.lsp.buf.rename, opts) -- smart rename
-  opts.desc = "show LSP references"
-  keymap.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
-  opts.desc = "show LSP type definitions"
-  keymap.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
-  opts.desc = "see available code actions"
-  keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
+  opts.desc = "Smart rename"
+  keys.set("n", "<leader>ln", vim.lsp.buf.rename, opts) -- smart rename
+  opts.desc = "Show LSP references"
+  keys.set("n", "<leader>lr", "<cmd>Telescope lsp_references<CR>", opts) -- show definition, references
+  opts.desc = "Show LSP type definitions"
+  keys.set("n", "<leader>lt", "<cmd>Telescope lsp_type_definitions<CR>", opts) -- show lsp type definitions
+  opts.desc = "See available code actions"
+  keys.set({ "n", "v" }, "<leader>lC", vim.lsp.buf.code_action, opts) -- see available code actions, in visual mode will apply to selection
 
-  opts.desc = "show buffer diagnostics"
-  keymap.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
-  opts.desc = "show line diagnostics"
-  keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
-  opts.desc = "go to previous diagnostic"
-  keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
-  opts.desc = "go to next diagnostic"
-  keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
-  opts.desc = "restart LSP"
-  keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
+  opts.desc = "Show buffer diagnostics"
+  keys.set("n", "<leader>lD", "<cmd>Telescope diagnostics bufnr=0<CR>", opts) -- show  diagnostics for file
+  opts.desc = "Show line diagnostics"
+  keys.set("n", "<leader>ld", vim.diagnostic.open_float, opts) -- show diagnostics for line
+  opts.desc = "Go to previous diagnostic"
+  keys.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+  opts.desc = "Go to next diagnostic"
+  keys.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+  opts.desc = "Restart LSP"
+  keys.set("n", "<leader>lR", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
 end
 
 function M.gitsigns_on_attach_callback(bufnr)
