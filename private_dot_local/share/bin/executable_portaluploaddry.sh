61cec4e01b40f7c1bfb53d886829566f642e0b41
FAZuH <fazuhhh@gmail.com>
2024-07-30 22:54:21 +0700

    Add dry portal sync scripts

---
+++ private_dot_local/share/bin/executable_portaluploaddry.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+/usr/bin/rclone sync --fix-case --create-empty-src-dirs --delete-after --verbose --transfers 4 \
+    --checkers 8 --contimeout 60s --timeout 300s --retries 3 --low-level-retries 10 --stats 1s \
+    --stats-file-name-length 0 --fast-list --dry-run /home/faz/Drive "gdrive:8 Portal"
